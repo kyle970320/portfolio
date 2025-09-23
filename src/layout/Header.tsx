7fb5414b6ed2d6bf5749c61e8fbfbc046559c521
minkyu_Park <kyle970320@farmzonesnc.com>
2025-09-23 13:28:47 +0900

    feat: add skills and header's progressbar

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -34,14 +34,18 @@ const headerItem = [
 ];
 export default function Header() {
   const [headerBg, setHeaderBg] = useState<string>("bg-transparent opacity-30");
-
+  const [progressBarWidth, setProgressBarWidth] = useState<string>("0%");
   useEffect(() => {
     const modifyBg = () => {
       const windowH = window.innerHeight;
       if (window.scrollY > windowH - 20) {
         setHeaderBg("bg-[#ffffff5a] backdrop-blur-md opacity-100");
+        setProgressBarWidth(
+          `${((window.scrollY + windowH) / document.body.scrollHeight) * 100}%`,
+        );
       } else {
         setHeaderBg("bg-transparent opacity-30");
+        setProgressBarWidth(`0%`);
       }
     };
     window.addEventListener("scroll", modifyBg);
@@ -51,13 +55,16 @@ export default function Header() {
       window.removeEventListener("resize", modifyBg);
     };
   }, []);
-
   return (
-    <div className="fixed hidden justify-center left-0 z-13 w-full py-10 px-5 sm:flex">
+    <div className="fixed hidden justify-center left-0 z-101 w-full py-10 px-5 sm:flex">
       <div
-        className={`flex justify-center items-center gap-5 py-2 px-4 ${headerBg} rounded-2xl transition-all duration-100`}
+        className={`relative flex justify-center items-center gap-5 py-2 px-4 ${headerBg} rounded-2xl transition-all duration-100 overflow-hidden`}
       >
-        <div className="flex items-center gap-6">
+        <div
+          className="absolute top-0 left-0 h-full w-10 bg-main rounded-2xl"
+          style={{ width: progressBarWidth }}
+        />
+        <div className="relative flex items-center gap-6 z-10">
           {headerItem.map((el, index) => {
             const key = `${el.title}_${index}`;
             return (
--- src/widgets/StackList.tsx
+++ src/widgets/StackList.tsx
@@ -1,45 +1,106 @@
 import { useEffect, useState } from "react";
-
+import { Tooltip } from "@minus-ui/core";
 const stackData = [
   {
     category: "front",
-    image: "/free1.jpg",
+    image: "/skills/javascript-original.svg",
+    content: "Javascript",
   },
   {
     category: "front",
-    image: "/free1.jpg",
-  },
-  {
-    category: "library",
-    image: "/free1.jpg",
-  },
-  {
-    category: "deploy",
-    image: "/free1.jpg",
-  },
-  {
-    category: "deploy",
-    image: "/free1.jpg",
-  },
-  {
-    category: "library",
-    image: "/free1.jpg",
+    image: "/skills/typescript-original.svg",
+    content: "Typescript",
   },
   {
     category: "front",
-    image: "/free1.jpg",
+    image: "/skills/react-original-wordmark.svg",
+    content: "React",
+  },
+  {
+    category: "front",
+    image: "/skills/header_logo.svg",
+    content: "React-Native",
+  },
+  {
+    category: "front",
+    image: "/skills/nextjs-2.svg",
+    content: "Next",
+  },
+  {
+    category: "library",
+    image: "/skills/zustand.webp",
+    content: "Zustand",
+  },
+  {
+    category: "library",
+    image: "/skills/redux-original.svg",
+    content: "Redux",
+  },
+  {
+    category: "library",
+    image: "/skills/sass-original.svg",
+    content: "Sass",
+  },
+  {
+    category: "library",
+    image: "/skills/Storybook.webp",
+    content: "StoryBook",
+  },
+  {
+    category: "library",
+    image: "/skills/Styled-components.webp",
+    content: "Styled-Components",
+  },
+  {
+    category: "library",
+    image: "/skills/tailwindcss-icon.svg",
+    content: "Tailwind",
+  },
+  {
+    category: "library",
+    image: "/skills/Tanstack-query.webp",
+    content: "Tanstack-Query",
+  },
+  {
+    category: "others",
+    image: "/skills/Vite.webp",
+    content: "Vite",
+  },
+  {
+    category: "others",
+    image: "/skills/firebase-icon.svg",
+    content: "Firebase",
+  },
+  {
+    category: "others",
+    image: "/skills/supabase.jpg",
+    content: "Supabase",
+  },
+  {
+    category: "others",
+    image: "/skills/github.webp",
+    content: "Github",
+  },
+  {
+    category: "others",
+    image: "/skills/jenkins-icon.svg",
+    content: "Jenkins",
   },
 ];
 export default function StackList() {
-  const [list, setList] = useState<{ [key: string]: Array<string> }>({});
+  const [list, setList] = useState<{
+    [key: string]: Array<{ image: string; content: string }>;
+  }>({});
   const [currentCategory, setCurrentCategory] = useState<string>("");
   const listArray = Object.entries(list);
 
   useEffect(() => {
-    const currentList: { [key: string]: Array<string> } = {};
+    const currentList: {
+      [key: string]: Array<{ image: string; content: string }>;
+    } = {};
     stackData.map((el) => {
       const category = el.category;
-      const image = el.image;
+      const image = { image: el.image, content: el.content };
 
       if (!currentList[category]) {
         currentList[category] = [];
@@ -92,23 +153,34 @@ export default function StackList() {
               className={`flex felx-col flex-wrap justify-center items-center gap-2 w-full ${isBlurClass()} transition-all duration-100`}
             >
               {values.map((images, idx) => {
-                const mapKey = `${images}_${idx}`;
+                const { image, content } = images;
+                const mapKey = `${image}_${idx}`;
 
                 return (
                   <div
                     key={mapKey}
                     className={`relative w-12 h-12 opacity-100 transition-all ease-out duration-100 cursor-pointer`}
                   >
-                    <img
-                      src={images}
-                      alt=""
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                        objectPosition: "center center",
-                      }}
-                    />
+                    <Tooltip
+                      contents={content}
+                      position="bottom"
+                      backgroundColor="rgba(66,66,66,1)"
+                      containerClassName="h-full"
+                      contentClassName="z-1000 text-white font-semibold"
+                    >
+                      <img
+                        src={image}
+                        className="p-0.5 rounded-xl"
+                        alt=""
+                        style={{
+                          backgroundColor: "rgba(255,255,255,1)",
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "contain",
+                          objectPosition: "center center",
+                        }}
+                      />
+                    </Tooltip>
                   </div>
                 );
               })}
